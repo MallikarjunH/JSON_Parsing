0760b333fe62bd84777dfd05529f65a8ea2cb9ec
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-11-08 12:21:14 +0530

    Alamofire + SwiftJSON - Base Project

---
+++ JSONParsing_Alamofire+SwiftyJSON/JSONParsing_Alamofire+SwiftyJSON/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONParsing_Alamofire+SwiftyJSON
+//
+//  Created by mallikarjun on 08/11/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func getLoginDetails(){
+        
+    }
+
+    
+    
+}
+
