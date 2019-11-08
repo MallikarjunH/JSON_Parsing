3f356be27d96cd5e2ee0cdc19203d803d14708a4
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-11-08 10:12:56 +0530

    Swift - URLSession Example Added

---
+++ Swift_NetworkRequest_URLSession/Swift_NetworkRequest_URLSession/ViewController.swift
@@ -0,0 +1,146 @@
+//
+//  ViewController.swift
+//  Swift_NetworkRequest_URLSession
+//
+//  Created by mallikarjun on 07/11/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var idArray = [Int]()
+    var titleArray = [String]()
+    
+    //Developer data
+    var idArrayForDeveloper = [Int]()
+    var nameArrayForDeveloper = [String]()
+    var salaryArrayForDeveloper = [Int]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func callApiButtonClicked(_ sender: Any) {
+  
+      // getAPICallExample1()
+       getAPICallExample2()
+        
+    }
+
+    
+    func getAPICallExample1(){
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/todos") else {return}
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+        guard let dataResponse = data,
+                  error == nil else {
+                  print(error?.localizedDescription ?? "Response Error")
+                  return }
+            do{
+                //here dataResponse received from a network request
+                let jsonResponse = try JSONSerialization.jsonObject(with:
+                                       dataResponse, options: [])
+                
+              //  print("JSON Response: \(jsonResponse)") //Response result
+                
+                //get the data from dictionary
+                
+                guard let jsonArray = jsonResponse as? [[String: Any]] else {
+                      return
+                }
+    
+              //  print("JSON Array: \(jsonArray)")
+                
+                if jsonArray.count > 0{
+                    
+                    self.idArray = []
+                    self.titleArray = []
+                    
+                    for dictData:Dictionary<String,Any> in jsonArray{
+                        
+                        let idValue:Int = dictData["id"] as? Int ?? 0
+                        let title:String = dictData["title"] as? String ?? "Not Title"
+                
+                        self.idArray.append(idValue)
+                        self.titleArray.append(title)
+                    }
+                    
+                    print("Id Array is: \(self.idArray)")
+                    print("Title Array is: \(self.idArray)")
+                    
+                }else{
+                    //array is empty
+                }
+                
+             } catch let parsingError {
+                print("Error", parsingError)
+           }
+        }
+        task.resume()
+        
+    }
+    
+    func getAPICallExample2(){
+        
+        guard let url = URL(string: "http://182.72.79.154/iphonetest/getTheData.php") else {return}
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+        guard let dataResponse = data,
+                  error == nil else {
+                  print(error?.localizedDescription ?? "Response Error")
+                  return }
+            do{
+                //here dataResponse received from a network request
+                let jsonResponse = try JSONSerialization.jsonObject(with:
+                                       dataResponse, options: [])
+                
+              //  print("JSON Response: \(jsonResponse)") //Response result
+                
+                //get the data from dictionary
+                guard let jsonDict = jsonResponse as? NSDictionary else {
+                      return
+                }
+                //print("JSON Dictionary: \(jsonDict)")
+                
+                //Get Array inside dictionary
+                
+               // guard let arrayData = jsonDict["result"] as? [[String:Any]] else {
+                guard let arrayData = jsonDict.object(forKey: "result") as? [[String:Any]] else {
+                      return
+                }
+             //   print("Array is: \(arrayData)") //array of dictionary
+                
+                if arrayData.count > 0{
+                    
+                    self.idArrayForDeveloper = []
+                    self.nameArrayForDeveloper = []
+                    self.salaryArrayForDeveloper = []
+                    
+                    for developerDataDict:Dictionary<String,Any> in arrayData{
+                        
+                        let userId:Int = developerDataDict["id"] as? Int ?? 0
+                        let name:String = developerDataDict["name"] as? String ?? "No name"
+                        let salary:Int = developerDataDict["salary"] as? Int ?? 0
+                        
+                        self.idArrayForDeveloper.append(userId)
+                        self.nameArrayForDeveloper.append(name)
+                        self.salaryArrayForDeveloper.append(salary)
+                    }
+                    
+                    print("Developer name Array: \(self.nameArrayForDeveloper)")
+                }
+                else{
+                    print("Array is empty")
+                }
+                
+             } catch let parsingError {
+                print("Error", parsingError)
+           }
+        }
+        task.resume()
+        
+    }
+}
+
